de296ad6e651d212ac388dc3ca0d3bb2f7380b43
Nerses228 <119117657+Nerses228@users.noreply.github.com>
2022-12-03 16:11:53 +0300

    Add files via upload

--- Crickets.cs
+++ Crickets.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Criticks : MonoBehaviour
+public class Crities : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       print("Crickets");
-       // Crickets
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Crickets");
+        // Crickets
     }
 }
